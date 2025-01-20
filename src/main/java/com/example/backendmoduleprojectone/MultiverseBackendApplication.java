385eadcdc859df0b02e489f2d03927b794bae075
David Dixon <daviddi@mo.co.uk>
2025-01-20 15:16:54 +0000

    project set up

---
+++ src/main/java/com/example/backendmoduleprojectone/MultiverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendmoduleprojectone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiverseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendmoduleprojectone/MultiverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendmoduleprojectone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
